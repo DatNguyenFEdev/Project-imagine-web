62baf279009f645c1954162767fdf7f033655ca4
DatNguyen <duydat10122001@gmail.com>
2021-05-20 21:16:07 +0700

    imagine-web

---
+++ script.js
@@ -0,0 +1,7 @@
+var listTestimonials = document.querySelector('testimonials');
+var testimonial = document.querySelectorAll('testimonial');
+
+function slider() {
+
+}
+testimonial.onclick('slider');
